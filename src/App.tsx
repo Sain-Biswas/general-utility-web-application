3624bf2ca269e711d2986c5e4517bd83225097a2
Sain-Biswas <sain.biswas614@gmail.com>
2025-02-14 10:19:53 +0530

    Add Dark Mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import ModeToggleButton from "./components/mode-toggle";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ModeToggleButton />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+import { useTheme } from "./provider/theme-provider";
+
+export default function ModeToggleButton() {
+  const { theme, setTheme } = useTheme();
+
+  const toggleMode = () => {
+    if (theme === "dark") setTheme("light");
+    else if (theme === "light") setTheme("dark");
+    else {
+      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+        .matches
+        ? "light"
+        : "dark";
+
+      setTheme(systemTheme);
+    }
+  };
+
+  return (
+    <Button variant="ghost" size="icon" onClick={toggleMode}>
+      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
